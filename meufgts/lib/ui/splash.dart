40873df96f45324ec56ddddc81a2423fc53b044b
Alifyz <alifyz@outlook.com>
2019-09-02 07:23:43 -0300

    add: flutter meusalario project

---
+++ meufgts/lib/ui/splash.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.deepPurple,
+      child: Column(
+        children: <Widget>[
+          Text("MeuFGTS")
+        ],
+      ),
+    );
+  }
+}
