3ba22cf0f410fa151e80834165eb82559412dc8f
Alifyz Pires <alifyzdev@gmail.com>
2019-10-27 15:10:08 -0300

    add: refactoring photogram

--- photogram/lib/main.dart
+++ photogram/lib/main.dart
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
         // or simply save your changes to "hot reload" in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.grey,
       ),
       home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
@@ -92,7 +92,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
-              'You have pushed the button this many times:',
+              'You have  the button this many times:',
             ),
             Text(
               '$_counter',
