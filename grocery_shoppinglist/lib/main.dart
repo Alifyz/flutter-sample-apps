7055eea4178ed5f9e035cf799c7cb00f989902e0
Alifyz Pires <alifyzdev@gmail.com>
2019-12-21 17:56:37 -0300

    add: initial grocery listing app

--- grocery_shoppinglist/lib/main.dart
+++ grocery_shoppinglist/lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.deepOrange,
+        primarySwatch: Colors.green,
       ),
       home: HomeScreen(),
     );
