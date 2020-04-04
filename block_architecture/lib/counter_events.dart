b2cf4c74520d3573e85206e7c906a81de1743319
Alifyz <alifyzdev@gmail.com>
2020-04-04 08:18:13 -0300

    refactor: folder structure and organization

--- block_architecture/lib/counter_events.dart
+++
@@ -1,4 +0,0 @@
-abstract class CounterEvents {}
-
-class IncrementEvent extends CounterEvents {}
-class DecrementEvent extends CounterEvents {}
--- investor/lib/res/strings.dart
+++
@@ -1,4 +0,0 @@
-class StringsResource {
-  static const String APP_NAME = "Perfil de Investimento";
-  static const String WELCOME_TEXT = "Alify";
-}
