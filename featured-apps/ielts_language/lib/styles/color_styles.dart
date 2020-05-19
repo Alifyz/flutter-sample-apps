4890e7c0fc5015898ffa2c1b3ebc7830e49c473c
Alifyz <alifyzdev@gmail.com>
2020-05-18 23:40:48 -0300

    add: initial colors and design

---
+++ featured-apps/ielts_language/lib/styles/color_styles.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class ColorStyles {
+  static const primaryColor = const Color(0xFFC63756);
+  static const secondaryColor = const Color.fromRGBO(240, 238, 240, 1.0);
+  static const primaryTextColor = Colors.white;
+  static const secondaryTextColor = const Color(0xFF2C2C2C);
+}
